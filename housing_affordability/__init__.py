ae07375dc99f8f1067e54fdd44b92c78c5284698
Ben Miller <bsmiller25@gmail.com>
2018-11-26 17:02:44 -0500

    adding apps.py

---
+++ housing_affordability/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'housing_affordability.apps.housingConfig'
---
+++ housing_affordability/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class housingConfig(AppConfig):
+    name = 'housing_affordability'
+    verbose_name = "Housing Affordability"
