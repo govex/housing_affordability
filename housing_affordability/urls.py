240c7e6301f4101bdd0128d795678c8f567dc6a6
Sara Bertran de Lis <sarabertrandelis@gmail.com>
2019-04-24 11:19:21 -0400

    Added dropdown for select city, table with general data in overview, page to select cities for comparison (partially done) and other small changes

--- housing_affordability/urls.py
+++ housing_affordability/urls.py
@@ -4,10 +4,12 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.affordability, name='affordability'),
-    path('select/', views.affordability_select, name='affordability_select'),
-    path('wwc/', views.affordability_wwc, name='affordability_wwc'),
-    path('overview/<int:gov_id>/', views.affordability_overview, name='affordability_overview'),
-    path('index/<int:gov_id>/', views.affordability_index, name='affordability_index'),
+    path('', views.home, name='housing_home'),
+    path('select/', views.affordability_select, name='housing_select'),
+    path('comp/', views.affordability_comp_select, name='housing_comp_select'),
+    path('comp/<int:comp_option>', views.affordability_comp, name='housing_comp'),
+    path('overview/<int:gov_id>/', views.affordability_overview, name='housing_overview'),
+    path('index/<int:gov_id>/', views.affordability_index, name='housing_affordability'),
+    path('about/', views.about, name='housing_about')
 
 ]
